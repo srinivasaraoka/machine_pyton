b93710d70da5b66be37ddc2a79f0c8b20c6bd93c
srinivasaraoka <63554925+srinivasaraoka@users.noreply.github.com>
2024-11-25 21:44:02 +0530

    Update 0_while_loop.py

--- 0_while_loop.py
+++ 0_while_loop.py
@@ -52,3 +52,11 @@ def maxPower(s: str) -> int:
     return max(max_power, current_power)
 
 print(maxPower('abbbbcccdddddd'))       
+#powerofthree
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        if  n<=0:
+            return False
+        while n%3==0:
+            n=n//3
+        return n==1        
